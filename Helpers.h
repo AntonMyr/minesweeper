f54342466e4c7ca02cdd9f310100c30706b4303d
antonmyr <15anmy@stockholmscience.se>
2018-10-10 15:51:49 +0200

    Added new function declarations for helper functions in PresentMap and UpdatePosition

--- Helpers.h
+++ Helpers.h
@@ -8,6 +8,17 @@ void initMap(int map[], int mines[]);
 void calcMines(int map[], int mines[], int width, int height);
 
 void positionCase(int option, int map[], int width, int mine);
+
 int checkAndAdd(int positionVal);
+
+void initTakenSquares(bool takenSquares[], int width, int height);
+
+bool isMine(int position, int map[]);
+
+bool isTaken(int position, bool takenSquares[]);
+
+int getPosition(int xCoord, int yCoord, int width);
+
+void updateStatus(int position, bool takenSquares[]);
 
 #endif // !HELPERS_H
