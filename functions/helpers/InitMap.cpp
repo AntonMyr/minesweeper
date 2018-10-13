6b9a0b41b0f84d8228fbb5cdf19d5b8f1f3dd64c
antonmyr <15anmy@stockholmscience.se>
2018-10-13 02:07:24 +0200

    No longer fills out 0's in map array

--- functions/helpers/InitMap.cpp
+++ functions/helpers/InitMap.cpp
@@ -3,20 +3,16 @@
 
 /**
  * SHOULD:
- *  Fill out map array with 0's.
  *
- *  Then switch out 0's to -1's based on
+ *  Switch out 0's to -1's based on
  *  positions in mines array
  *
  * @params:
  *  int map[], the map array
  *  int mines[], the mine array containing mine positions
  */
-void initMap(int map[], int mines[]) {
-  for (int i = 0; i < 81; i++){
-    map[i] = 0;
-  }
-  for (int i = 0; i < 10; i++) {
+void initMap(int map[], int mines[], int minesAmount) {
+  for (int i = 0; i < minesAmount; i++) {
     map[mines[i]] = -1;
   }
 }
