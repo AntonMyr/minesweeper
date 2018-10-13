3c739221d24a138b974c0d16808e414a2197d23d
antonmyr <15anmy@stockholmscience.se>
2018-10-13 02:04:05 +0200

    Removed comment, added new argument

--- functions/CreateMap.cpp
+++ functions/CreateMap.cpp
@@ -5,12 +5,11 @@
 void CreateMap(int map[], int width, int height) {
 
   const int MINES_AMOUNT = 10;
-  // loop through map with i = height*width?
 
   int mines[MINES_AMOUNT];
   setMines(mines, MINES_AMOUNT);
 
-  initMap(map, mines);
+  initMap(map, mines, MINES_AMOUNT);
 
   calcMines(map, mines, width, height);
 
