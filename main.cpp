57dd0d008c790b15e95b8fe9f1e50769ed4f8b58
antonmyr <15anmy@stockholmscience.se>
2018-10-13 02:05:19 +0200

    Sorted everything out and added loops

--- main.cpp
+++ main.cpp
@@ -3,21 +3,49 @@
 
 int main() {
 
-  int xCoord = 7, yCoord = 0;
+  int xCoord, yCoord;
+  UpdateResult status;
+  bool isEnded;
 
-  int map[80];
-  bool takenSquares[80];
+  int map[81] = { 0 };
+  bool takenSquares[81] = { false };
 
   CreateMap(map);
-  PresentMap(map, takenSquares);
-  UpdateResult status = UpdatePosition(map, takenSquares, xCoord, yCoord);
 
-  std::cout << status << std::endl;
-  for(int i = 0; i < 81; i++) {
-    if(i % 9 == 0) {
-      std::cout << std::endl;
+  do {
+    PresentMap(map, takenSquares);
+
+    std::cout << "Choose x coordinate: ";
+    std::cin >> xCoord;
+    while(xCoord > 8 || xCoord < 0 || std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(10, '\n');
+      std::cout << "You need to pick a x coordinate between 0 and 8: ";
+      std::cin >> xCoord;
     }
-    std::cout << takenSquares[i] << " ";
+
+    std::cout << "\nChoose y coordinate: ";
+    std::cin >> yCoord;
+    while(yCoord > 8 || yCoord < 0 || std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(10, '\n');
+      std::cout << "You need to pick a y coordinate between 0 and 8: ";
+      std::cin >> yCoord;
+    }
+
+    status = UpdatePosition(map, takenSquares, xCoord, yCoord);
+    isEnded = CheckForEnd(map, takenSquares);
+    std::cout << "I AM ENDSTATUS: " << isEnded << std::endl;
+
+  } while( !isEnded && (status != positionIsMine));
+
+  if(status == positionIsMine) {
+    PresentMap(map, takenSquares);
+    std::cout << "\n\t\tYOU LOST" << std::endl;
+  } else if(isEnded) {
+    PresentMap(map, takenSquares);
+    std::cout << "\n\t\tYOU WON" << std::endl;
   }
+
   return 0;
 }
