c146dd469911e6738a2cd0b893b05d2c1d9c8c43
antonmyr <15anmy@stockholmscience.se>
2018-10-13 02:06:26 +0200

    now updates status if it's a mine

--- functions/UpdatePosition.cpp
+++ functions/UpdatePosition.cpp
@@ -7,6 +7,7 @@ UpdateResult UpdatePosition(int map[], bool takenSquares[], int xCoord, int yCoo
   int position = getPosition(xCoord, yCoord, width);
 
   if(isMine(position, map)) {
+    updateStatus(position, takenSquares);
     return positionIsMine;
   }
   else if(isTaken(position, takenSquares)) {
